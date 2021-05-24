e396985598c684e627ffb9b9ec3f7c1ee7730107
shelley cavaness <shelleycavaness@yahoo.com>
2021-05-24 18:02:20 +0200

    got rid of the gendre of the form request

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,14 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="https://">here</a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          shellyeah {new Date().getFullYear()} All rights reserved. Crafted by{' '}
+          <a href="https://">shellyeah</a>.
         </span>
       </div>
     </footer>
